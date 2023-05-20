1970ff008073f094b0c6df00f41063a82e7cbba5
Igor_Maylov <metal_92@list.ru>
2023-05-21 01:07:24 +0300

    done 4.3

--- test_commit.py
+++ test_commit.py
@@ -1,3 +1,3 @@
-mac = "AAAA:BBBB:CCCC"
-res = mac.replace(':', '.')
-print(res)
+config = "switchport trunk allowed vlan 1,3,10,20,30,100"
+result = config.split()
+print((config.split()[-1]).split(','))
