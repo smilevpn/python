854a4d6a11134807dae0ca5851f7cf553f8da9d4
Igor_Maylov <metal_92@list.ru>
2023-05-21 01:11:26 +0300

    done 4.4

--- test_commit.py
+++ test_commit.py
@@ -1,3 +1,3 @@
-config = "switchport trunk allowed vlan 1,3,10,20,30,100"
-result = config.split()
-print((config.split()[-1]).split(','))
+vlans = [10, 20, 30, 1, 2, 100, 10, 30, 3, 4, 10]
+result = sorted(set(vlans))
+print(result)
