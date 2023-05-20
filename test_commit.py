a53c46932f2894ea45ea53957c4cacfd95c6e6b8
Igor_Maylov <metal_92@list.ru>
2023-05-21 00:49:30 +0300

    done 4.2

--- test_commit.py
+++ test_commit.py
@@ -1,4 +1,3 @@
-import sys
-nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
-res = nat.replace('FastEthernet0/1', 'GigabitEthernet')
+mac = "AAAA:BBBB:CCCC"
+res = mac.replace(':', '.')
 print(res)
