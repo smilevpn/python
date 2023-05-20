3f11042ea153f8ba818ba8305e588bf0a3819193
Igor_Maylov <metal_92@list.ru>
2023-05-20 21:49:18 +0300

    install ipython

--- test_commit.py
+++ test_commit.py
@@ -1,9 +1,9 @@
 a = 10
 b = 5
 if a > b:
-    print("A больше B")
-    print(a - b)
+	print("A больше B")
+	print(a - b)
 else:
-    print("B больше или равно A")
-    print(b - a)
+	print("B больше или равно A")
+	print(b - a)
 print("Конец")
