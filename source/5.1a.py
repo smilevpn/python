39cb0fa02e47a8fb486432a57bccccd453bf99ba
Igor Maylov <metal_92@list.ru>
2023-05-29 12:49:47 +0300

    done 5.1a task

---
+++ source/5.1a.py
@@ -0,0 +1,29 @@
+london_co = {
+"r1": {
+"location": "21 New Globe Walk",
+"vendor": "Cisco",
+"model": "4451",
+"ios": "15.4",
+"ip": "10.255.0.1"
+},
+"r2": {
+"location": "21 New Globe Walk",
+"vendor": "Cisco",
+"model": "4451",
+"ios": "15.4",
+"ip": "10.255.0.2"
+},
+"sw1": {
+"location": "21 New Globe Walk",
+"vendor": "Cisco",
+"model": "3850",
+"ios": "3.6.XE",
+"ip": "10.255.0.101",
+"vlans": "10,20,30",
+"routing": True
+}
+}
+
+interface = input('Введите имя сетевого интерфейса r1|r2|sw1: ')
+parameter = input('Введите имя конкретного параметра location|vendor|model|ios|ip|vlans|routing: ')
+print((london_co[interface])[parameter])
