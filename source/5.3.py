37d1fd4a502625e565e49c17543491eb2ddd14ff
Igor_Maylov <metal_92@list.ru>
2023-06-16 22:14:03 +0300

    edit 5.3 task

--- source/5.3.py
+++ source/5.3.py
@@ -1,29 +1,24 @@
-# def sum_interfaces():
-#     sum_int = ''
-#     for key in dict.keys():
-#         sum_int += key + ' '
-#     return sum_int[:-1]
-
-dict = {
-'access':
-    "switchport mode access\n"
-    "switchport access vlan {vlans}\n"
-    "switchport nonegotiate\n"
-    "spanning-three portfast\n"
-    "spanning-three bpduguard enable\n",
-'trunk':
-    "switchport trunk encapsulation dot1q\n"
-    "switchport mode trunk\n"
-    "switchport trunk allowed vlan {vlans}\n"
-}
-
-
 print('Введите режим работы интерфейса (access/trunk): ')
 mode = input()
 print('Введите тип и номер интерфейса: ')
 interface = input()
 print('Введите номер влан(ов): ')
 vlans = input()
+dict = {
+    "access": {
+        "switchport mode access",
+        f"switchport access vlan {vlans}",
+        "switchport nonegotiate",
+        "spanning-three portfast",
+        "spanning-three bpduguard enable", 
+        },
+    "trunk": {
+        "switchport trunk encapsulation dot1q",
+        "switchport mode trunk",
+        f"switchport trunk allowed vlan {vlans}",
+        }
+    }
+
 
 print('interface ' + interface + '\n')
 print(dict[mode])
